69dc3ba31d580ed070188e22689ac870d6c89658
Maksymilian Babarowski <maksymilian@babarowski.pl>
2019-06-11 16:21:17 +0200

    User interface for postfix2infix. Minor readimprov

--- postfix2infix.c
+++ postfix2infix.c
@@ -4,17 +4,35 @@
 #include <string.h>
 #include "stack_2d.h"
 
+// TODO: handle input size dynamically
+#define BUFFER_SIZE 100
+
 char* postfix2infix(const char*);
 int isOp(char);
 
 int main(int argc, char* argv[]) {
-    printf("%s\n", postfix2infix("ab*c+"));
+    if(argc > 0) {
+        for(int i = 1; i < argc; ++i) {
+            printf("%s\n", postfix2infix(argv[i]));
+        }
+    } else {
+        char buffer[BUFFER_SIZE];
+        int c, i = 0;
+        while((c = getchar()) != EOF) {
+            if(c == '\n') {
+                buffer[i] = '\0';
+                printf("%s\n", postfix2infix(buffer));
+                i = 0;
+            } else {
+                buffer[i++] = c;
+            }
+        }
+    }
     return 0;
 }
 
 char* postfix2infix(const char* input_exp) {
-    size_t len = strlen(input_exp);
-    Stack2D* stack = create2DStack(len);
+    Stack2D* stack = create2DStack(strlen(input_exp));
 
     const char* ptr = input_exp;
 
@@ -27,7 +45,7 @@ char* postfix2infix(const char* input_exp) {
             char* operand2 = pop(stack);
             char* operand1 = pop(stack);
 
-            char* toPush = (char*)malloc(strlen(operand1) + strlen(operand2) + 4); // 2 brackets, 2 expressions, operator and null-terminator
+            char* toPush = (char*)malloc(strlen(operand1) + strlen(operand2) + 4); // 2 expressions, 2 brackets, operator and null-terminator
             sprintf(toPush, "(%s%c%s)", operand1, *ptr, operand2);
             push(stack, toPush);
         }
